8b1028f2c7c287af522410c2ffa2d72eeb1fff2b
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-06-05 22:23:01 +0600

    react router added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwPMf_sIrOSVNTWa50UsMpNkWQkFTcVR8",
+  authDomain: "aircnc-98c4a.firebaseapp.com",
+  projectId: "aircnc-98c4a",
+  storageBucket: "aircnc-98c4a.appspot.com",
+  messagingSenderId: "810129632439",
+  appId: "1:810129632439:web:8616a413671b3730c67132"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
